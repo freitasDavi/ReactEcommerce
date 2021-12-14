589ae2461604623eaf6f56faa54e0b637035091c
Davi <davi.freitas@useall.com.br>
2021-12-14 16:33:58 -0300

    created slider component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Announcement from "../components/Announcement";
 import NavBar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
     return (
         <div>
             <Announcement />
             <NavBar />
+            <Slider />
         </div>
     );
 }
