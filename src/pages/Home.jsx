1c4c16b0e2d235c8e097170342c0a0ac5e4eda2e
Davi <davi.freitas@useall.com.br>
2021-12-15 10:42:41 -0300

    Created categories component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Announcement from "../components/Announcement";
 import NavBar from "../components/Navbar";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Announcement />
             <NavBar />
             <Slider />
+            <Categories />
         </div>
     );
 }
